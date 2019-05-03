c1ada4991e5da38a800ef63ae7b0aeb8b08bf7a9
Clark McAdoo <cmcadoo@gsandf.com>
2019-05-03 13:13:03 -0500

    adding query to shortcode

--- atl_ad_shortcode.php
+++ atl_ad_shortcode.php
@@ -2,7 +2,20 @@
 
 //[Zone Shortcode]
 function include_zone( $atts ){
-	return $atts['slug'];
+	$params = [
+		'post_type' => 'atl_ad',
+		'tax_query' => [
+			[
+				'taxonomy' => 'zones',
+				'field' => 'slug',
+				'terms' => $atts['slug']
+			]
+		]
+	];
+
+	$ad = get_posts($params);
+
+	return $ad[0] -> post_title;
 }
 add_shortcode( 'ad_zone', 'include_zone' );
 
